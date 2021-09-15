a34925e2072adbd2584fe9524e4a821c6e3a3b04
DJBooker2 <problemchildprogramming@gmail.com>
2021-09-15 19:13:32 -0400

    -- Demo Prep -- DJ Booker

---
+++ Info-Tech Projects I/fileUploadAction.php
@@ -0,0 +1,15 @@
+<!--
+    This php action will handle the user querying the entire database
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Name Insert Action</title>
+</head>
+
+<body>
+    You would like to encrypt file <?php echo $_GET["fileToUpload"]; ?>?<br />
+</body>
+
+</html>
--- Info-Tech Projects I/loginAction.php
+++ Info-Tech Projects I/loginAction.php
@@ -4,53 +4,16 @@
     This page will 
 -->
 
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-$target_dir = "N_E_E_D_T_O_M_A_K_E"; // Need to establish a directory
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+<head>
+    <title>Login Action</title>
+</head>
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
+<body>
+    Username is  <?php echo $_GET["uName"]; ?>?<br />
+    Password is  <?php echo $_GET["passwd"]; ?>?<br />
+</body>
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
-}
-?>
+</html>
