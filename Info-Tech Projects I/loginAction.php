5e7543aacb7b701d4f28387bc9a03b80101caf73
DJBooker2 <problemchildprogramming@gmail.com>
2021-09-17 14:14:05 -0400

    -- Code Merging -- DJ Booker

--- Info-Tech Projects I/loginAction.php
+++ Info-Tech Projects I/loginAction.php
@@ -12,8 +12,43 @@
 </head>
 
 <body>
-    Username is  <?php echo $_GET["uName"]; ?>?<br />
-    Password is  <?php echo $_GET["passwd"]; ?>?<br />
+     <?php
+
+        echo "Username is: ".$_GET["uName"];
+        echo "Password is: ". $_GET["passwd"];
+
+        /*
+        Edited By: Mackinnon Jackson
+        Date Last Edited: 9/15/2021
+        Purpose: To add in each user
+       
+        include_once 'My_DB_Functions.php';
+
+        // Connect to database
+        My_Connect_DB();
+
+        // The query looks through the entire table to make a total amount of rows
+        $query = "select * from $db_tablename";
+        $res = $db->prepare($query);
+        $res->execute();
+        $num = $res->rowCount();
+
+        //  Both the username and password gets added into the sql statement to be executed
+        $username = $_GET['uName'];
+        $password = $_GET['passwd'];
+        $sql = "INSERT INTO users(userID,username,password) values($num,'" . $username . "','" . $password . "')";
+
+        $result = $db->prepare($sql);
+        $result->execute();
+
+
+
+        echo "Added user";
+
+        echo "$num";
+        */
+        
+        ?>
 </body>
 
 </html>
