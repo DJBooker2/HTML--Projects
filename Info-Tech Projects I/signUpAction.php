2cdcc2c76176d5629681cef8e77a86ac72ccd5b8
DJBooker2 <problemchildprogramming@gmail.com>
2021-09-17 10:16:47 -0400

    -- User Error Handling -- DJ Booker

--- Info-Tech Projects I/signUpAction.php
+++ Info-Tech Projects I/signUpAction.php
@@ -1,7 +1,7 @@
 <!--
     Programmed By: DJ Booker
     Sept 9, 2021
-    This page will 
+    This page will handle the user creating a account and add it to the database 
 -->
 
 <!DOCTYPE html>
@@ -13,13 +13,21 @@
 
 
 <body>
-
-
     <?php
+    // Variable to hold the output message
+    $outputMessage = " ";
+
     // Check to see if passwords match
     // If they do the input is saved
-    if ($_GET["passwd2"] != $_GET["passwd"]) {
-        echo "Please make sure both passwords match!";
+    if (empty($_GET["uName"])) {
+        $outputMessage = "You must enter a username";
+        echo $outputMessage;
+    } elseif (empty($_GET["passwd"]) || empty($_GET["passwd2"])) {
+        $outputMessage = "Please Enter a password!";
+        echo $outputMessage;
+    } elseif ($_GET["passwd2"] != $_GET["passwd"]) {
+        $outputMessage = "Please make sure both passwords match!";
+        echo $outputMessage;
     } else {
         echo "Your username: " . $_GET["uName"] . "<br/>";
         echo "First Password: " . $_GET["passwd"] . "<br/>";
